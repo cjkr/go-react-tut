b501043964170af9d7de977272a32ec6411ce76e
JKR <jcheruiyot11@gmail.com>
2024-06-03 20:57:12 +0300

    finalizing API calls in UI

--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,31 +1,30 @@
 import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
-import React, { useState } from "react";
 import TodoItem from "./TodoItem";
+import { useQuery } from "@tanstack/react-query";
+
+export type Todo = {
+  _id: number;
+  body: string;
+  completed: boolean;
+};
 
 export default function TodoList() {
-  const [isLoading, setIsLoading] = useState(false);
-  const todos = [
-    {
-      _id: 1,
-      body: "Buy groceries",
-      completed: true,
+  const { data: todos, isLoading } = useQuery<Todo[]>({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/todos");
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data || [];
+      } catch (error) {
+        console.log(error);
+      }
     },
-    {
-      _id: 2,
-      body: "Shave hair",
-      completed: false,
-    },
-    {
-      _id: 3,
-      body: "Walk dog",
-      completed: false,
-    },
-    {
-      _id: 4,
-      body: "Read book",
-      completed: true,
-    },
-  ];
+  });
 
   return (
     <>
